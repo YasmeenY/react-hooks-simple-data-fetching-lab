acb421360108226332933376fa7d0ccc7b45ee7e
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-04-17 09:36:12 -0400

    tests passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useEffect, useState} from 'react';
+
+function App(){
+    const [dogImage, setDogImage] = useState(null)
+    useEffect(()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(randomDog => setDogImage(randomDog.message))
+    }, [])
+    if (!dogImage){
+        return(
+            <p>
+                Loading...
+            </p>
+        )
+    }
+    else {
+        return (
+            <img alt='A Random Dog' src={dogImage}></img>
+        )
+    }
+}
+
+export default App;
